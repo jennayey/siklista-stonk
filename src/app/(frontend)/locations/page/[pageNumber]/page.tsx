f2435107cd6511b1075b628eea5099b71ca931f4
JENNAPC\jenna <jennayey@gmail.com>
2025-02-10 22:36:37 +0800

    fix

--- src/app/(frontend)/locations/page/[pageNumber]/page.tsx
+++ src/app/(frontend)/locations/page/[pageNumber]/page.tsx
@@ -30,7 +30,7 @@ export default async function Page({ params: paramsPromise }: Args) {
     depth: 1,
     limit: 12,
     page: sanitizedPageNumber,
-    overrideAccess: false,
+    // overrideAccess: false,
   })
 
   return (
@@ -73,7 +73,7 @@ export async function generateStaticParams() {
   const payload = await getPayload({ config: configPromise })
   const { totalDocs } = await payload.count({
     collection: 'locations',
-    overrideAccess: false,
+    // overrideAccess: false,
   })
 
   const totalPages = Math.ceil(totalDocs / 10)
