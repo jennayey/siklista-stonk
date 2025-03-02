1951eb65b0ca3a0d0aeb0d4f7200e03b8e2b48f3
JENNAPC\jenna <jennayey@gmail.com>
2025-03-02 11:17:35 +0800

    fix: Replaced default description and image OG meta text that is added to pages

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -3,14 +3,14 @@ import { getServerSideURL } from './getURL'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  description: 'An open-source website built with Payload and Next.js.',
+  description: ' Find Establishments with Bike Parking in the Philippines',
   images: [
     {
-      url: `${getServerSideURL()}/website-template-OG.webp`,
+      url: `${getServerSideURL()}/siklista-website-OG.png`,
     },
   ],
-  siteName: 'Payload Website Template',
-  title: 'Payload Website Template',
+  siteName: 'Siklista Parking PH',
+  title: 'Siklista Parking PH',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
