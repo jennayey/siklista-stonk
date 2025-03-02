2b52269697bfc5c8910a4199f9be8959b473199b
JENNAPC\jenna <jennayey@gmail.com>
2025-03-02 13:46:50 +0800

    fix cms route

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -25,6 +25,9 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  routes: {
+    admin: '/crustcontrol' 
+  },
   admin: {
     components: {
       // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
