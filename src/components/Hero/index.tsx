b098631211c268984233744c2a6936ed31d610ca
JENNAPC\jenna <jennayey@gmail.com>
2025-03-04 21:30:10 +0800

    Updated styles

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -10,7 +10,7 @@ export const Hero: React.FC<Props> = (props) => {
   const { title, foldingBikeFriendly } = props
 
   return (
-    <div className="bg-night  w-full ">
+    <div className="bg-gradient-to-r from-night to-nightlite w-full">
       <div className="container pt-12 lg:pt-16 pb-24 lg:pb-20 relative">
         <h2 className="text-4xl md:text-5xl w-3/4	 font-semibold text-slime">{title}</h2>
         {foldingBikeFriendly ? <div className="absolute bottom-6 right-10 rotate-[-10deg]"><FoldingBadge /></div> : null}
--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -9,7 +9,7 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default:
-          "border-transparent bg-slime text-night hover:bg-success/80",
+          "border-transparent bg-night text-white hover:bg-success/80",
         secondary:
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:
