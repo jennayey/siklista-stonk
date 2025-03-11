df0cfadc3bf79698c44b1781fd85db097cba4893
JENNAPC\jenna <jennayey@gmail.com>
2025-03-11 22:38:40 +0800

    fix: uses smaller logo on small device sizes (sm below)

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -15,15 +15,27 @@ export const Logo = (props: Props) => {
 
   return (
     /* eslint-disable @next/next/no-img-element */
-    <img
-      alt="Siklista Parking"
-      width={250}
-      height={34}
-      loading={loading}
-      fetchPriority={priority}
-      decoding="async"
-      className={clsx('max-w-[175px] md:max-w-[12.5rem] w-full h-[34px]', className)}
-      src="https://raw.githubusercontent.com/jennayey/siklista-stonk/refs/heads/master/public/logo-full-dark.svg"
-    />
+    <div>
+      <img
+        alt="Siklista Parking"
+        width={250}
+        height={34}
+        loading={loading}
+        fetchPriority={priority}
+        decoding="async"
+        className={clsx('hidden md:block max-w-[175px] md:max-w-[12.5rem] w-full h-[34px]', className)}
+        src="https://raw.githubusercontent.com/jennayey/siklista-stonk/refs/heads/master/public/logo-full-dark.svg"
+      />
+      <img
+        alt="Siklista Parking"
+        width={250}
+        height={34}
+        loading={loading}
+        fetchPriority={priority}
+        decoding="async"
+        className={clsx('md:hidden max-w-[175px] md:max-w-[12.5rem] w-full h-[34px]', className)}
+        src="https://raw.githubusercontent.com/jennayey/siklista-stonk/refs/heads/master/public/logo-icon-dark.svg"
+      />
+    </div>
   )
 }
